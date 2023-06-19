2b33e12958019e1afac7d77116cffd954da57b15
Md Meyad Hossen <122221406+MdMeyadHossen@users.noreply.github.com>
2023-06-20 02:30:34 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// @Author=Md Meyad Hossen
+$(document).ready(function() {
+    $('#particles').particleground({
+      dotColor: '#5cbdaa',
+      lineColor: '#5cbdaa'
+    });
+    $('.intro').css({
+      'margin-top': -($('.intro').height() / 2)
+    });
+  });
+
